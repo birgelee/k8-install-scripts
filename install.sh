b07647367347a0da86180f7b675ca1402efb9454
Henry Birge-Lee <birgelee@princeton.edu>
2024-11-20 13:58:45 -0500

    new

--- install.sh
+++ install.sh
@@ -22,3 +22,47 @@ echo "Disable Swap"
 
 swapoff -a
 sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+
+echo "Install br_netfilter Module"
+lsmod | grep br_netfilter
+modprobe br_netfilter
+lsmod | grep br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sysctl --system
+
+echo "Configure docker"
+
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+echo "Docker reload"
+systemctl daemon-reload
+systemctl enable docker
+systemctl restart docker
+systemctl status docker
+
+echo "install kubeadm, kubelet, and kubectl"
+apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
